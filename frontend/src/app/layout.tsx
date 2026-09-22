5a9f549ced8a3a78a9e81962603309accf6c5fd0
Tonii18 <ajsanrosal@gmail.com>
2026-09-22 15:31:23 +0200

    Vercel analytics enabled

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/navbar";
+import { Analytics } from "@vercel/analytics/next"
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -40,6 +41,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       <body className={`${poppins.variable} font-sans antialiased`}>
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
